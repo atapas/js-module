b3ca28cbc47b16764bf2ffc33b3b376df7d53117
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-05-14 13:56:27 +0530

    imported module into html directly

---
+++ src/image.js
@@ -0,0 +1,20 @@
+
+const getUser = () => {
+
+    return [
+        {
+            'name': 'Alex',
+            'age': 12
+        },
+        {
+            'name': 'Brahm',
+            'age': 34
+        },
+        {
+            'name': 'Colin',
+            'age': 78
+        }
+    ]
+};
+
+export { getUser };
