6a81b8902b675536ae00c1da1e0e9cc9c09cdfdf
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-05-14 13:29:02 +0530

    aggregating imports

---
+++ src/combine.js
@@ -0,0 +1,3 @@
+
+export * as Calc from './calc.js';
+export {default as name, sayGreeting} from './whoami.js';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 
-import * as Calc from './calc.js';
-import {default as name, sayGreeting} from './whoami.js';
+import * as Combine from './combine.js';
 
-console.log('The Sum is', Calc.sum(2,3));
-console.log('The Sub is', Calc.sub(5,3));
+console.log('The Sum is', Combine.Calc.sum(2,3));
+console.log('The Sub is', Combine.Calc.sub(5,3));
 
-console.log('The name is', name());
-console.log('Greeting is', sayGreeting('Tapas'));
+console.log('The name is', Combine.name());
+console.log('Greeting is', Combine.sayGreeting('Tapas'));
