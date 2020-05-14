14098618bb6e9f81cf7eb144a538ea899a727ab7
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-05-14 12:03:18 +0530

    export together and use of aliases

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,9 @@
-export const sum = (a, b) => {
+const sum = (a, b) => {
     return a + b;
 };
 
-export const sub = (a,b) => {
+const sub = (a,b) => {
     return a - b;
 };
+
+export { sum, sub };
